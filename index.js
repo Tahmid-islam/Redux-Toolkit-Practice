2056a9f4280891f6f1f0e135fa5e0deada0bbb9b
Tahmid Islam <tahmid231@gmail.com>
2022-07-23 23:14:54 +0600

    update

---
+++ index.js
@@ -0,0 +1,13 @@
+const store = require("./app/store");
+const { cakeActions } = require("./features/cake/cakeSlice");
+const { icecreamActions } = require("./features/icecream/icecreamSlice");
+
+console.log("Initial State", store.getState());
+const unsubscribe = store.subscribe(() => {
+//   console.log("Updated State", store.getState());
+});
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.restocked(20));
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.restocked(2));
+unsubscribe();
