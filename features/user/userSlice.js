829fc6fbfbc6d9c897a917fa16567d97ccdf9c31
Tahmid Islam <tahmid231@gmail.com>
2022-07-24 13:06:52 +0600

    update asyn action

--- features/user/userSlice.js
+++ features/user/userSlice.js
@@ -1,6 +1,6 @@
-const createSlice = require("@reduxjs/toolkit");
+const createSlice = require("@reduxjs/toolkit").createSlice;
 const createAsyncThunk = require("@reduxjs/toolkit").createAsyncThunk;
-const axios = require('axios')
+const axios = require("axios");
 
 const initialState = {
   loading: false,
@@ -8,20 +8,32 @@ const initialState = {
   error: "",
 };
 
-const fetchUsers = createAsyncThunk('user/fetchUsers')
+// Generates pending, fulfilled and rejected action types
+const fetchUsers = createAsyncThunk("user/fetchUsers", () => {
+  return axios
+    .get("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.data.map((user) => user.id));
+});
 
 const userSlice = createSlice({
   name: "user",
   initialState,
-  reducers: {
-    requested: (state) => {
+  extraReducers: (builder) => {
+    builder.addCase(fetchUsers.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
       state.loading = false;
-    },
-    success: (state) => {
+      state.users = action.payload;
+      state.error = "";
+    });
+    builder.addCase(fetchUsers.rejected, (state, action) => {
       state.loading = false;
-    },
-    failure: (state) => {
-      state.loading = false;
-    },
+      state.error = action.error.message;
+      state.users = [];
+    });
   },
 });
+
+module.exports = userSlice.reducer;
+module.exports.fetchUsers = fetchUsers;
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const store = require("./app/store");
 const { cakeActions } = require("./features/cake/cakeSlice");
 const { icecreamActions } = require("./features/icecream/icecreamSlice");
+const { fetchUsers } = require("./features/user/userSlice");
+
 
 console.log("Initial State", store.getState());
 const unsubscribe = store.subscribe(() => {
   console.log("Updated State", store.getState());
 });
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.restocked(20));
-store.dispatch(icecreamActions.ordered());
-store.dispatch(icecreamActions.restocked(2));
-unsubscribe();
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.restocked(20));
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.restocked(2));
+store.dispatch(fetchUsers())
+// unsubscribe();
